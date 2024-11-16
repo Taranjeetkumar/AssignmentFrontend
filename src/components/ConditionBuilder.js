f7fef75651c30bff9e98a8520d894ea39b32ad76
sicsdev <sicsdev21@gmail.com>
2024-11-16 13:55:18 +0530

    latest

--- src/components/ConditionBuilder.js
+++ src/components/ConditionBuilder.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import '../App.css'
+import React, { useEffect } from 'react';
+import '../App.css';
 
 function ConditionBuilder({ conditions, setConditions, setConditionType }) {
     const addCondition = () => {
-        setConditions([...conditions, { field: 'totalSpending', operator: '>', value: '' }]);
+        setConditions([...conditions, { field: 'totalSpending', operator: '>', value: '',conditionType:"" }]);
+    };
+
+    const removeCondition = (index) => {
+        const updatedConditions = conditions.filter((_, i) => i !== index);
+        setConditions(updatedConditions);
     };
 
     const handleConditionChange = (index, key, value) => {
@@ -12,13 +17,17 @@ function ConditionBuilder({ conditions, setConditions, setConditionType }) {
         setConditions(newConditions);
     };
 
-    console.log("condition : : ", conditions);
+    useEffect(() => {
+        console.log("Updated conditions: ", conditions);
+    }, [conditions]);
+
     return (
         <div className='conditionContainer'>
             <h3>Define Audience Segment</h3>
             {conditions.map((condition, index) => (
                 <div key={index} className='conditionRow'>
                     <select
+                        value={condition.field}
                         onChange={(e) => handleConditionChange(index, 'field', e.target.value)}
                         className='select'
                     >
@@ -27,6 +36,7 @@ function ConditionBuilder({ conditions, setConditions, setConditionType }) {
                         <option value="lastVisit">Last Visit</option>
                     </select>
                     <select
+                        value={condition.operator}
                         onChange={(e) => handleConditionChange(index, 'operator', e.target.value)}
                         className='select'
                     >
@@ -36,28 +46,32 @@ function ConditionBuilder({ conditions, setConditions, setConditionType }) {
                     </select>
                     <input
                         type="number"
+                        value={condition.value}
                         onChange={(e) => handleConditionChange(index, 'value', e.target.value)}
                         placeholder="Value"
                         className='input'
                     />
+                    <div className='conditionRow'>
+                        <p>Select Condition to apply:</p>
+                        <select
+                            onChange={(e) => {setConditionType(e.target.value); handleConditionChange(index, 'conditionType', e.target.value)}}
+                            className='select'
+                        >
+                            <option value="">Select</option>
+                            <option value="and">AND</option>
+                            <option value="or">OR</option>
+                        </select>
+                    </div>
+                    <div className='conditionRow'>
+                        <button onClick={() => removeCondition(index)} className='addButton'>
+                            Remove
+                        </button>
+                    </div>
                 </div>
             ))}
-
-            {
-                conditions.length > 0 ? (
-                    <div  className='conditionRow'>
-                    <p> Select Codition to apply :</p>
-                    <select
-                        onChange={(e) => setConditionType(e.target.value)}
-                        className='select'
-                    >
-                        <option value="and">AND</option>
-                        <option value="or">OR</option>
-                    </select>
-                    </div>
-                ): ""
-      }
-            <button onClick={addCondition} className='addButton'>Add Condition</button>
+            <button onClick={addCondition} className='addButton'>
+                Add Condition
+            </button>
         </div>
     );
 }
